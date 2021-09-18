1ec0a7a3cdafb052177d5cb34985f169fcff23b0
Anil Singh <asjadoun@gmail.com>
2021-09-18 07:04:28 -0400

    Added new aggregation method with Discriminator function

--- src/main/scala/com/st/aggregation/AggregatorFS2.scala
+++ src/main/scala/com/st/aggregation/AggregatorFS2.scala
@@ -46,6 +46,8 @@ object AggregatorFS2 {
 
           def producer: Stream[F, Unit] =
             in.map(x => x.asRight.some).evalMap(queue.offer).onFinalize(queue.offer(None))
+//            in.map(x => x.asRight.some).parEvalMap(10)(queue.offer).onFinalize(queue.offer(None))
+//          in.map(x => x.asRight.some).parEvalMapUnordered(10)(queue.offer).onFinalize(queue.offer(None))
 
           def emitNonEmpty(c: Chunk.Queue[O]): Stream[F, Chunk[O]] =
             if (c.size > 0) Stream.emit(c)
@@ -82,6 +84,87 @@ object AggregatorFS2 {
                         }
                     }
                 }
+            }
+
+          startTimeout
+            .flatMap(token => go(Chunk.Queue.empty, token, 0, None).concurrently(producer))
+            .onFinalize {
+              currentTimeout.modify {
+                case (cancelInFlightTimeout, _) =>
+                  (F.unit, true) -> cancelInFlightTimeout
+              }.flatten
+            }
+      }
+
+  def groupWithInUsingDiscriminatorFunction[F[_], O](maxBucketSize: Int, maxWaitTime: FiniteDuration)
+                                                    (elementWeight: O => Long)(elementClassifier: O => String)
+                                                    (implicit F: Async[F], timer: Temporal[F]): Pipe[F, O, Chunk[O]] =
+    in => {
+      Stream
+        .eval {
+          Queue
+            .synchronous[F, Option[Either[Token, O]]]
+            .product(Ref[F].of(F.unit -> false))
+        }
+    }
+      .flatMap {
+        case (queue, currentTimeout) =>
+          def startTimeout: Stream[F, Token] =
+            Stream
+              .eval(F.delay(new Token))
+              .evalTap {
+                newToken =>
+                  val timeout: F[Unit] = timer.sleep(maxWaitTime) >> queue.offer(newToken.asLeft.some)
+                  timeout.start
+                    .bracket(_ => F.unit) { fiber =>
+                      currentTimeout.modify {
+                        case st@(cancelInFlightTimeout, streamTerminated) =>
+                          if (streamTerminated)
+                            st -> fiber.cancel
+                          else
+                            (fiber.cancel, streamTerminated) -> cancelInFlightTimeout
+                      }.flatten
+                    }
+              }
+
+          def producer: Stream[F, Unit] =
+            in.map(x => x.asRight.some).evalMap(queue.offer).onFinalize(queue.offer(None))
+
+          def emitNonEmpty(c: Chunk.Queue[O]): Stream[F, Chunk[O]] =
+            if (c.size > 0) Stream.emit(c)
+            else Stream.empty
+
+          def go(acc: Chunk.Queue[O], currentTimeout: Token, accSize: Long, accAttribute: Option[String]): Stream[F, Chunk[O]] =
+            Stream.eval(queue.take).flatMap {
+              case None => emitNonEmpty(acc)
+              case Some(element: Either[Token, O]) =>
+                element match {
+                  case Left(timeout) if timeout == currentTimeout =>
+                    emitNonEmpty(acc) ++ startTimeout.flatMap { newTimeout =>
+                      go(Chunk.Queue.empty, newTimeout, 0, None)
+                    }
+                  case Left(timeout) if timeout != currentTimeout => go(acc, currentTimeout, 0, None)
+                  case Right(nextElement) =>
+                    val nextAttribute: String = elementClassifier(nextElement)
+                    val nextAttributeSize: Long = elementWeight(nextElement)
+                    val runningAccumulatorAttribute: Option[String] = if (acc.size > 0) accAttribute else Some(nextAttribute)
+                    val runningBucketSize: Long = accSize + nextAttributeSize
+
+                    if (runningBucketSize < maxBucketSize && runningAccumulatorAttribute.contains(nextAttribute)) {
+                      go(acc :+ Chunk(nextElement), currentTimeout, runningBucketSize, runningAccumulatorAttribute)
+                    }
+                    else if (runningBucketSize == maxBucketSize && runningAccumulatorAttribute.contains(nextAttribute)) {
+                      Stream.empty ++ startTimeout.flatMap { newTimeout =>
+                        go(acc :+ Chunk(nextElement), newTimeout, runningBucketSize, runningAccumulatorAttribute)
+                      }
+                    } else {
+                      val stream: Stream[Pure, Chunk[O]] = if (acc.isEmpty) Stream.empty else Stream.emit(acc)
+                      stream ++ startTimeout.flatMap { newTimeout =>
+                        go(Chunk.Queue(Chunk(nextElement)), newTimeout, nextAttributeSize, Some(nextAttribute))
+                      }
+                    }
+                }
+              case _ => Stream.raiseError[F](new RuntimeException("unknown error"))
             }
 
           startTimeout
--- src/test/scala/com/st/aggregation/AggregationExtSpec.scala
+++ src/test/scala/com/st/aggregation/AggregationExtSpec.scala
@@ -3,36 +3,58 @@ package com.st.aggregation
 import cats.effect.IO
 import cats.effect.unsafe.IORuntime
 import com.st.aggregation.AggregationFS3.groupWithin
-import com.st.aggregation.AggregatorFS2.groupWithIn
-import fs2.Stream
+import com.st.aggregation.AggregatorFS2.{groupWithIn, groupWithInUsingDiscriminatorFunction}
+import fs2.{Chunk, Stream}
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.duration.DurationInt
+import scala.util.Random
 
-class AggregationLogicSpec extends AnyFunSpec {
+class AggregationLogicSpec extends AnyFunSpec with Matchers {
 
   implicit val runtime: IORuntime = cats.effect.unsafe.IORuntime.global
   describe("The AggregationLogic"){
 
-    it("should chunk by key and limit"){
+    it("should aggregate by key and limit by time & size"){
       val list1 = List(Record(1, 3, "ABC", "BLACK"), Record(2, 7, "XYZ-ABC", "BLACK"), Record(3, 4, "HHHH", "BLACK"))
       val list2 = List(Record(4, 1, "Z" , "ORANGE"), Record(5, 9, "123456789", "ORANGE"), Record(6, 3, "MxN", "ORANGE"))
       val list3 = List(Record(7, 2, "EF", "BLACK"), Record(8, 8, "WZ-YZ-BC", "BLACK"))
-      val input = list1 ++ list2 ++ list3
+      val input: List[Record] = list1 ++ list2 ++ list3
 
-      val stream = Stream.fromIterator[IO](input.iterator, 1024)
+      val stream: Stream[IO, Record] = Stream.fromIterator[IO](input.iterator, 1024)
 
-      val result = stream
+      val result: List[Chunk[Record]] = stream
         .map(x => (x, x.flag, x.size))
-//        .through(groupWithin[IO, Record](12, 5.second))
         .through(groupWithIn[IO, Record](12, 5.second))
-        .map{c => println(c); c}
         .compile
         .toList
         .unsafeRunSync()
 
-      println(s"Total chunks: ${result.size}")
-      println(s"Result: $result")
+      val attributes: Set[String] = result.flatMap(c => c.head.map(_.flag)).toSet
+      attributes.size shouldBe 2
+      result.size shouldBe 5
+    }
+
+    it("should use functions to aggregate by key and limit by time & size"){
+      val record: String = Random.nextString(1024*2).mkString
+      val list: Seq[String] = (0 until 6000000).map(_ => record)
+
+      val stream: Stream[IO, String] = Stream.fromIterator[IO](list.iterator, 1024)
+
+      val result: List[Chunk[String]] = stream
+        .through(
+          groupWithInUsingDiscriminatorFunction[IO, String](1000000, 59.second)(elementWeight = _ => 1L)(elementClassifier = _ => "fixed")
+        )
+//        .map{c => println(s"${java.time.LocalDateTime.now()} Chunk size: ${c.size}"); c}
+        .compile
+        .toList
+        .unsafeRunSync()
+
+      val attributes: Set[String] = result.flatMap(c => c.head).toSet
+      attributes.size shouldBe 1
+      result.size shouldBe 6
+      result.map(_.size).sum shouldBe 6000000
     }
 
   }
